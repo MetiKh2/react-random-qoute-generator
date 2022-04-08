2aff7ed8bf54b144ac7b797f1d2ed755e7768a59
meti <mahdikhodarahimi0@gmail.com>
2022-04-09 00:02:36 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
+  const [advice, setAdvice] = useState('')
+  const url='	https://api.adviceslip.com/advice';
+  useEffect(()=>{
+    fetchAdvice()
+  },[])
+  const fetchAdvice=()=>{
+    axios.get(url).then(res=>{
+      setAdvice(res.data.slip.advice)
+    }).catch(err=>{
+      setAdvice(err)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button onClick={fetchAdvice} className="button">
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
     </div>
   );
 }
